5ff26a96d2e9bd51691f778510967c6afe29d486
Theo <theo.piedade@gmail.com>
2023-07-09 10:29:21 -0300

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import '../src/css/style.css'
+import "../src/css/style.css"
+import palavras from "./palavras";
+import Letras from "./Letras";
+
+const alfabeto = [
+  "A","B","C","D","E","F","G","H","I",
+  "J","K","L","M","N","O","P","Q","R",
+  "S","T","U","V","W","X","Y","Z"
+]
 
 function App() {
   return (
     <div className="Container">
       <div className="containerForkAndButton">
-        <img src='/assets/forca0.png'/>
+        <img src="/assets/forca0.png"/>
         <button>Escolher palavra</button>
+      </div>
+      <div className="containerWords">
+        {
+      alfabeto.map( letra => 
+                <Letras key={letra} letra={letra} />
+             )
+        }
       </div>
     </div>
   );
