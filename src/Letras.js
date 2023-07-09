ec7e41f2b177f57d372ce3a56e1e60c6b341d37d
Theo <theo.piedade@gmail.com>
2023-07-09 10:45:06 -0300

    Layout Start complete

--- src/Letras.js
+++ src/Letras.js
@@ -1,6 +1,7 @@
 
 export default function Letras (props) {
+    console.log("props.letra:"+props.letra);
     return (
-        <button>props.letra</button>
+        <button>{ props.letra }</button>
     );
 }
