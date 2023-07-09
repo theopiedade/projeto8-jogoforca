a3031b970a780f3f3fdc4742fac8bd02aea8ea83
Theo <theo.piedade@gmail.com>
2023-07-09 12:48:21 -0300

    Game Start initing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import palavras from "./palavras";
 import Letras from "./Letras";
 import { useState } from "react";
 
+const img = [
+  "forca0.png", "forca1.png", "forca2.png", "forca3.png", 
+  "forca4.png", "forca5.png", "forca6.png"
+]
+
 const alfabeto = [
   "A","B","C","D","E","F","G","H","I",
   "J","K","L","M","N","O","P","Q","R",
@@ -10,22 +15,35 @@ const alfabeto = [
 ]
 
 function App() {
-  const [disabledState, setDisabledState] = useState("true");
-  return (
-    <div className="container">
-      <div className="containerForkAndButton">
-        <img src="/assets/forca0.png"/>
-        <button>Escolher palavra</button>
+  const [disabledState, setDisabledState] = useState(true);
+  const [gameState, stateGameState] = useState("waiting");
+  const [gameCount, setgameCount] = useState(0);
+  const [gameImg, setGameImg] = useState(img[0]);
+
+  function GameStart() {
+    setDisabledState(false);
+    stateGameState("playing");
+    setgameCount(0);
+    setGameImg(img[0]);
+  }
+
+    console.log("gameState:"+gameState);
+    return (
+      <div className="container">
+        <div className="containerForkAndButton">
+          <img src={`/assets/img/`+gameImg}/>
+          <button onClick={() => GameStart()}>Escolher palavra</button>
+        </div>
+        <div className="containerWords">
+          {
+        alfabeto.map( letra => 
+                  <Letras key={letra} letra={letra} state={disabledState} />
+              )
+          }
+        </div>
       </div>
-      <div className="containerWords">
-        {
-      alfabeto.map( letra => 
-                <Letras key={letra} letra={letra} state={disabledState} />
-             )
-        }
-      </div>
-    </div>
-  );
+    );
+
 }
 
 export default App;
--- src/Letras.js
+++ src/Letras.js
@@ -1,6 +1,5 @@
 
 export default function Letras (props) {
-    console.log("props.letra:"+props.letra);
     return (
         <button disabled={props.state}>{ props.letra }</button>
     );
